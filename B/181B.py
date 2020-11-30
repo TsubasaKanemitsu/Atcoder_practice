2c544b5fa99288b28645f0d8f0fa492431fe96f5
TsubasaKanemitsu <is0209xx@gmail.com>
2020-11-30 20:33:24 +0800

    resolve 181B

---
+++ B/181B.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+sm = 0
+
+for i in range(N):
+    A, B = list(map(int, input().split()))
+    sm += B * (B + 1) / 2 - A * (A - 1) / 2
+print(int(sm))
+
+# 計算量を意識する
+# 数列の総和の計算式を上手く利用し，ACに収まる計算量にする
