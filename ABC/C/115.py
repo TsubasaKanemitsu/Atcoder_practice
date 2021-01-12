04d9e8babbc0eddebfcaf9e8ac4cb13dc22c2416
TsubasaKanemitsu <is0209xx@gmail.com>
2021-01-12 22:09:42 +0800

    resolve 115

---
+++ ABC/C/115.py
@@ -0,0 +1,12 @@
+import itertools
+
+n, k = list(map(int, input().split()))
+
+h = [int(input()) for _ in range(n)]
+h.sort(reverse=True)
+diff = 10 ** 9
+for i in range(n - k + 1):
+    temp_diff = h[i] - h[i + k - 1]
+    diff = min(diff, temp_diff)
+
+print(diff)
