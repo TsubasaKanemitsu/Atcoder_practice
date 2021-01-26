56822bc7bd588d43d69684f0da4462f769b680b9
TsubasaKanemitsu <is0209xx@gmail.com>
2021-01-26 23:01:31 +0800

    resolve 122 C

--- virtual_contest/20210126/c.py
+++ virtual_contest/20210126/c.py
@@ -1,3 +1,23 @@
 n, q = list(map(int, input().split()))
 s = input()
+l = []
+r = []
+for i in range(q):
+    _l, _r = list(map(int, input().split()))
+    l.append(_l)
+    r.append(_r)
 
+count_list = [0 for i in range(n)]
+count = 0
+for i in range(n - 1):
+    if s[i] == 'A' and s[i + 1] == 'C':
+        count_list[i] = count
+        count += 1
+        count_list[i + 1] = count
+    else:
+        count_list[i] = count
+        count_list[i + 1] = count
+# print(count_list)
+
+for i in range(q):
+    print(count_list[r[i] - 1] - count_list[l[i] - 1])
