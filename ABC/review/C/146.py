ed8057e8857b3932fb69d70d7654902d75dfea2e
TsubasaKanemitsu <is0209xx@gmail.com>
2021-02-28 21:59:41 +0800

    review binary search 146C

---
+++ ABC/review/C/146.py
@@ -0,0 +1,23 @@
+a, b, x = list(map(int, input().split()))
+
+n = 10 ** 9
+
+# 桁数出力
+def d(n):
+    n = str(n)
+    return len(n)
+
+left = 1
+right = n + 1
+
+if a * left + b * d(left) > x:
+    print(0)
+    exit()
+
+while right - left > 1:
+    mid = (right + left) // 2
+    if a * mid + b * d(mid) <= x:
+        left = mid
+    else:
+        right = mid
+print(left)
